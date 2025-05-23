2523c940cd8546839d851b7226bdecae4c7e3f39
MariaRayane15 <rayanenascimento64255@gmail.com>
2025-05-23 17:27:24 +0000

    Aula 23/05/2025

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,24 @@ import "./styles/global.css";
 import { MyText } from "./components/MyText";
 
 export default function App() {
+  const texts = [
+    { title: "Meu título", text: "Meu texto" },
+    { title: "Meu título", text: "Meu texto" },
+    { title: "Meu título", text: "Meu texto" },
+    { title: "Meu título", text: "Meu texto" },
+    { title: "Meu título", text: "Meu texto" },
+  ];
+
   return (
-    <div>
-      <h1>Meu primeiro React App</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ipsum,
-        aliquid alias nemo consectetur dignissimos delectus quos nisi deleniti
-        obcaecati vitae dolorem fuga, neque placeat laborum deserunt maiores
-        mollitia? Excepturi?
-      </p>
-    </div>
     // React Fragment
-    <>  
-      <MyText />
-      <MyText />
-      <MyText />
+     <>
+      {texts.map((text, index) => {
+        return (
+          <MyText key={index} title={`${text.title} ${index + 1}`}>
+            {text.text}
+          </MyText>
+        );
+      })}
     </>
   );
 }
-
-
-
-// import "./styles/theme.css";
-// import "./styles/global.css";
-// import { MyText } from "./components/MyText";
-
-// export default function App() {
-//   return (
-//     <div>
-//       <h1>Meu primeiro React App</h1>
-//       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-//         Quod quas error aliquam eligendi sit in est, a repellendus cupiditate. 
-//         Atque sunt quam culpa inventore dolor dolorum neque suscipit reprehenderit nihil!</p>
-//     </div>
-
-    
-//   );
-// }
