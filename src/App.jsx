8b31d4e8addc728d352f3ddfff5fb18e1fb69e53
MariaRayane15 <rayanenascimento64255@gmail.com>
2025-06-25 20:41:00 +0000

    Aula 25/06

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import { MyTextList } from "./components/MyTextList";
 import { Header } from "./components/Header";
-import { Main } from "./components/Main";
-import { Footer } from "./components/Footer";
+import { LuckyNumber } from "./components/LuckyNumber";
 
 export default function App() {
   return (
     <>
       <div className="container">
-        <Header />
-        <Main />
-        <Footer />
+        <LuckyNumber />
       </div>
     </>
   );
