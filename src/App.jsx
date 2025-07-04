a3b2e27ed0466d5c80bdfc077b4fd6c2121dfdc3
MariaRayane15 <rayanenascimento64255@gmail.com>
2025-07-04 23:34:56 +0000

    Aula 04/07

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import { Header } from "./components/Header";
-import { LuckyNumber } from "./components/LuckyNumber";
+import { ProductList } from "./components/ProductList";
 
 export default function App() {
   return (
     <>
-      <div className="container">
-        <LuckyNumber />
+      <div>
+        <ProductList />
       </div>
     </>
   );
