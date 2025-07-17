e4922d0e571b537e89d7c28f565ced656ec3a07d
MariaRayane15 <rayanenascimento64255@gmail.com>
2025-07-17 17:28:14 +0000

    Aula 17/07

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
 import styles from "./Header.module.css";
-import { Github, Instagram, Phone } from "lucide-react";
-export function Header() {
-  // Desestruturação de props
+
+export function Header({ cart }) {
   return (
-    <header className={styles.header1}>
-      <h1>Foco, Força, Fé</h1>
+    <header className={styles.header}>
+      <h1>TRJ Megastore</h1>
+      <div>
+        {cart.length > 0 && <p>{cart.length} products</p>}
+        <p>
+          Total $: {cart.reduce((total, product) => total + product.price, 0).toFixed(2)}
+        </p>
+      </div>
     </header>
   );
 }
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
-import { CircularProgress } from "@mui/material";
 import styles from "./ProductList.module.css";
+import { CircularProgress } from "@mui/material";
+import { Product } from "./Product";
 
-export function ProductList() {
-  const category = "smartphones";
-  const limit = 12;
-  const apiUrl = `https://dummyjson.com/products/category/${category}?limit=${limit}&select=id,thumbnail,title,price,description`;
+export function ProductList({ addToCart }) {
+  var category = "beauty";
+  var limit = 12;
+  var apiUrl = `https://dummyjson.com/products/category/${category}?limit=${limit}&select=id,thumbnail,title,price,description`;
 
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -16,55 +17,39 @@ export function ProductList() {
       try {
         const response = await fetch(apiUrl);
         const data = await response.json();
+
         setProducts(data.products);
-      } catch (err) {
-        setError(err);
+      } catch (error) {
+        setError(error);
       } finally {
         setLoading(false);
       }
     }
-
-    setTimeout(fetchProducts, 2000);
-  }, [apiUrl]);
-
+    setTimeout(() => {
+      fetchProducts();
+    }, 2000);
+  }, []);
   return (
     <div className={styles.container}>
-      <h1>TRJ Megastore</h1>
-
-      {}
+      
       <div className={styles.grid}>
         {products.map((product) => (
-          <div key={product.id} className={styles.card}>
-            <img
-              src={product.thumbnail}
-              alt={product.title}
-              className={styles.image}
-            />
-
-            <h2 className={styles.title}>{product.title}</h2>
-
-            <p className={styles.description}>{product.description}</p>
-
-            <p className={styles.price}>${product.price}</p>
-
-            {}
-            <button className={styles.button}>Adicionar ao carrinho</button>
-          </div>
+          <Product key={product.id} product={product} addToCart={addToCart} />
         ))}
       </div>
-
-      {}
       {loading && (
         <div>
           <CircularProgress
+            // size="sm"
             thickness={5}
             style={{ margin: "2rem auto", display: "block" }}
-            sx={{ color: "#6c3bb8" }}
+            sx={{
+              color: "#001111",
+            }}
           />
           <p>Loading products...</p>
         </div>
       )}
-
       {error && <p>Error loading products: {error.message}</p>}
     </div>
   );
